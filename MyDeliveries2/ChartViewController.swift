63ec2c5b124e2ac1a2e0028825f36d0a8f9addb2
Douglas Maltby <doug.maltby@gmail.com>
2020-03-01 21:40:31 -0600

    Add ChartViewController as subclass of Fiori based FUIChartFloorplanViewController. Tutorial 5, step 1.

---
+++ MyDeliveries2/ChartViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ChartViewController.swift
+//  MyDeliveries2
+//
+//  Created by Douglas Maltby on 3/1/20.
+//  Copyright © 2020 SAP. All rights reserved.
+//
+
+import UIKit
+// add SA Fiori libraries
+import SAPFoundation
+import SAPFiori
+import SAPCommon
+
+// Change subclass from iOS UIViewController to FUIChartFloorplanViewController
+// class ChartViewController: UIViewController {
+class ChartViewController: FUIChartFloorplanViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
