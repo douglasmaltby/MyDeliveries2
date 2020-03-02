ce768af06d087ffe027d10826004fffb0dbf37f7
Douglas Maltby <doug.maltby@gmail.com>
2020-03-01 22:55:36 -0600

    Create map view controller, add storyboard and segue. Tutorial 7 Steps 1-2

---
+++ MyDeliveries2/TrackingMapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TrackingMapViewController.swift
+//  MyDeliveries2
+//
+//  Created by Douglas Maltby on 3/1/20.
+//  Copyright © 2020 SAP. All rights reserved.
+//
+
+import UIKit
+
+class TrackingMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
